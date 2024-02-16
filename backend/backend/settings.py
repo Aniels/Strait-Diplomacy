4448fe1884aacd4a65dbb02b7f634ad514ce6f86
Aniels <Niels.Chiu@gmail.com>
2024-02-16 21:35:05 +0800

    seeting

--- backend/backend/settings.py
+++ backend/backend/settings.py
@@ -39,6 +39,8 @@ INSTALLED_APPS = [
     "django.contrib.staticfiles",
     # graphql
     "graphene_django",
+    # apps
+    "users",
 ]
 
 MIDDLEWARE = [
@@ -117,6 +119,8 @@ STATIC_URL = "static/"
 
 DEFAULT_AUTO_FIELD = "django.db.models.BigAutoField"
 
+
+AUTH_USER_MODEL = 'users.User'
 
 GRAPHENE = {
     "SCHEMA": "backend.schema.schema",
