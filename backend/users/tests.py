4c972b4bce6c3c58a8c319610bb2f70f6070168a
Aniels <Niels.Chiu@gmail.com>
2024-02-16 22:25:55 +0800

    simple UserGraphQLTest

--- backend/users/tests.py
+++ backend/users/tests.py
@@ -1,6 +1,7 @@
 from django.test import TestCase
 from .models import User
 
+
 class UserCreationTestCase(TestCase):
     def test_create_user(self):
         # Create a user
@@ -9,3 +10,33 @@ class UserCreationTestCase(TestCase):
         # Check if the user was created successfully
         self.assertEqual(user.username, 'testuser')
         self.assertTrue(user.check_password('testpassword'))
+
+
+from graphene.test import Client
+from .schema import user_schema
+
+class UserGraphQLTest(TestCase):
+    def test_user_query(self):
+        # Arrange
+        client = Client(user_schema)
+
+        # Act
+        executed = client.execute('''
+            query {
+                user(id: 1) {
+                    id
+                    username
+                    email
+                }
+            }
+        ''')
+
+        # Assert
+        expected_data = {
+            "user": {
+                "id": "1",
+                "username": "your_username",
+                "email": "your@email.com",
+            }
+        }
+        self.assertEqual(executed['data'], expected_data)
